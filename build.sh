811057a7345aff07f3f47e040cb9c89a0728c640
RafaelReyesCarmona <rafael.reyes.carmona@gmail.com>
2024-05-18 01:23:07 +0200

    Update ghest Ubuntu 18.04

--- build.sh
+++ build.sh
@@ -209,14 +209,14 @@ case "${PLATFORM}" in
 		#TOOLS=$ROOT/toolchain/arm-eabi-4.8-toolchain/bin/arm-eabi-
 		#TOOLS=$ROOT/toolchain/arm-eabi-4.7/bin/arm-eabi-
 
-  		if [ $( grep -E '^VERSION_ID=' /etc/os-release | awk -F '"' '{print $2}') == "16.04" ]; then
-			TOOLS=/usr/bin/arm-none-eabi-
-		else
+  		#if [ $( grep -E '^VERSION_ID=' /etc/os-release | awk -F '"' '{print $2}') == "16.04" ]; then
+		#	TOOLS=/usr/bin/arm-none-eabi-
+		#else
   			#TOOLS=$ROOT/toolchain/arm-eabi-4.7/bin/arm-eabi-
-     			TOOLS=$ROOT/toolchain/arm-eabi-4.8-toolchain/bin/arm-eabi-
-     		fi
+     		#	TOOLS=$ROOT/toolchain/arm-eabi-4.8-toolchain/bin/arm-eabi-
+     		#fi
 
-		#TOOLS=$ROOT/toolchain/arm-linux-androideabi-4.7/bin/arm-linux-androideabi-
+		TOOLS=$ROOT/toolchain/arm-eabi-4.7/bin/arm-eabi-
 		KERNEL_NAME="linux3.4.67"
 		;;
 	*)
